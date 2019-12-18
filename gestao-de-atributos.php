d3966def2895f50066f4f31b550ae90bea26d1e4
FabianaRBarros <53559035+FabianaRBarros@users.noreply.github.com>
2019-12-18 19:43:45 +0000

    sdaddsds

--- gestao-de-atributos.php
+++ gestao-de-atributos.php
@@ -38,92 +38,100 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
                 </thead>
                 <tbody>
                 <?php
-
-                    // Definição da query a ser executada posteriormente
-                    $query_attributes = "SELECT attribute.name, attribute.obj_id, attribute.id, attribute.value_type, attribute.form_field_name, attribute.form_field_type, attribute.unit_type_id, attribute.form_field_order, attribute.form_field_size, attribute.mandatory, attribute.state
-                    FROM `attribute`, `object`
-                    WHERE attribute.obj_id = object.id";            
                 
+                $query_objeto="SELECT DISTINCT attribute.obj_id, object.name
+                                FROM `attribute`, `object`
+                                WHERE attribute.obj_id = object.id";
+
+                $result_objeto = executa_query($query_objeto);  
+
+                $lines_objeto = mysqli_num_rows($result_objeto);   
+
+                // ciclo que percorre o array associativo
+                while ($array_objeto = mysqli_fetch_array($result_objeto)) { 
+                    
+                    // Definição da query a ser executada posteriormente
+                    $query_attributes = "SELECT DISTINCT attribute.name, attribute.obj_id, attribute.id, attribute.value_type, attribute.form_field_name, attribute.form_field_type, attribute.unit_type_id, attribute.form_field_order, attribute.form_field_size, attribute.mandatory, attribute.state
+                    FROM `attribute`, `object`
+                    WHERE attribute.obj_id = '{$array_objeto['obj_id']}'";            
+
                     // Utiliza a função executa_query existente no ficheiro common.php e executa a query na base de dados
-                    $result_attributes = executa_query($query_attributes);     
+                    $result_attributes = executa_query($query_attributes); 
 
                     // Utiliza a função do mysql para saber o número de linhas necessário em cada célula
-                    $lines_attributes = mysqli_num_rows($result_attributes); 
+                    $lines_attributes = mysqli_num_rows($result_attributes);?>
 
-                        // Criação de um array com os valores da query guardados na variável $result_attributes
-                        while ($array_attributes = mysqli_fetch_array($result_attributes)) {              
-                            ?>
-                            <!-- Definição número colunas e linhas-->
-                            <tr colspan="1" rowspan="<?php echo $lines_attributes?>">
-                                <td><?php
-                                 $query_obj = "SELECT DISTINCT name
-                                 FROM object
-                                 WHERE id='{$array_attributes['obj_id']}'";
+                    <!-- Definição número colunas e linhas-->
+                    <tr>
+                        <td  colspan="1" rowspan="<?php echo $lines_attributes;?>"><?php echo $array_objeto['name'];?>
+                        </td>
 
-                                $resultado_obj = executa_query($query_obj); // Passa $query_obj como parametro de executa_query e coloca o resultado na variavel $resultado_obj
+                    <?php 
+                    
+                    // Criação de um array com os valores da query guardados na variável $result_attributes
+                    while ($array_attributes = mysqli_fetch_array($result_attributes)) {              
+                        ?>
+                        <td><?php
+                            echo $array_attributes['id']; ?>
+                            </td>
+                        <td><?php
+                            echo $array_attributes['name']; ?>
+                            </td>
+                        <td><?php
+                            echo $array_attributes['value_type']; ?>
+                            </td>
+                        <td><?php
+                            echo $array_attributes['form_field_name']; ?>
+                            </td>
+                        <td><?php
+                            echo $array_attributes['form_field_type']; ?>
+                            </td>
+                        <td><?php
+                            if ($array_attributes['unit_type_id'] != null) { // Faz a verificação para ver se há unidades
+                                // A query vai selecionar as unidades(name) da tabela attr_unit_type e e comparar o id com o id que está no array
+                                $query_tipo_unidade = "SELECT DISTINCT name
+                                                            FROM attr_unit_type
+                                                            WHERE id='{$array_attributes['unit_type_id']}'";
 
-                                $array_obj = mysqli_fetch_assoc($resultado_obj);
-                                echo $array_obj['name'];?>
-                                </td>
-                                <td><?php
-                                    echo $array_attributes['id']; ?>
-                                    </td>
-                                <td><?php
-                                    echo $array_attributes['name']; ?>
-                                    </td>
-                                <td><?php
-                                    echo $array_attributes['value_type']; ?>
-                                    </td>
-                                <td><?php
-                                    echo $array_attributes['form_field_name']; ?>
-                                    </td>
-                                <td><?php
-                                    echo $array_attributes['form_field_type']; ?>
-                                    </td>
-                                <td><?php
-                                    if ($array_attributes['unit_type_id'] != null) { // Faz a verificação para ver se há unidades
-                                        // A query vai selecionar as unidades(name) da tabela attr_unit_type e e comparar o id com o id que está no array
-                                        $query_tipo_unidade = "SELECT DISTINCT name
-                                                                    FROM attr_unit_type
-                                                                    WHERE id='{$array_attributes['unit_type_id']}'";
+                                $resultado_tipo_unidade = executa_query($query_tipo_unidade); // Passa $query_tipo_unidade como parametro de executa_query e coloca o resultado na variavel $resultado_tipo_unidade
 
-                                        $resultado_tipo_unidade = executa_query($query_tipo_unidade); // Passa $query_tipo_unidade como parametro de executa_query e coloca o resultado na variavel $resultado_tipo_unidade
-
-                                        $array_tipo_unidade = mysqli_fetch_assoc($resultado_tipo_unidade);
-                                        echo $array_tipo_unidade['name'];
-                                    // Se não existem unidades coloca o (-)
-                                    } else { 
-                                        echo '-';
-                                    } ?>
-                                </td>
-                                <td><?php
-                                    echo $array_attributes['form_field_order']; ?>
-                                </td>
-                                <td><?php
-                                    echo $array_attributes['form_field_size']; ?>
-                                </td>
-                                 <?php
-                                    if ($array_attributes['mandatory'] == 1) {?>
-                                        <td>sim</td>
-                                    <?php 
-                                    } else { ?>
-                                        <td>não</td>
-                                        <?php 
-                                    } ?>
-                                <td><?php
-                                    if ($array_attributes['state'] == "active") { // Se o estado for ativo escreve o que está abaixo 
-                                        ?>Activo</td> 
-                                        <td>[editar][desactivar]</td>
-                                        <?php
-                                    } else { // Se o estado for inativo escreve o que está abaixo
-                                        ?>Inactivo</td>
-                                        <td>[editar][activar]</td>
-                                        <?php
-                                    }?>
-                                </td>
-                            </tr>
+                                $array_tipo_unidade = mysqli_fetch_assoc($resultado_tipo_unidade);
+                                echo $array_tipo_unidade['name'];
+                            // Se não existem unidades coloca o (-)
+                            } else { 
+                                echo '-';
+                            } ?>
+                        </td>
+                        <td><?php
+                            echo $array_attributes['form_field_order']; ?>
+                        </td>
+                        <td><?php
+                            echo $array_attributes['form_field_size']; ?>
+                        </td>
                             <?php
-                        }?>
+                            if ($array_attributes['mandatory'] == 1) {?>
+                                <td>sim</td>
+                            <?php 
+                            } else { ?>
+                                <td>não</td>
+                                <?php 
+                            } ?>
+                        <td><?php
+                            if ($array_attributes['state'] == "active") { // Se o estado for ativo escreve o que está abaixo 
+                                ?>Activo</td> 
+                                <td>[editar][desactivar]</td>
+                                <?php
+                            } else { // Se o estado for inativo escreve o que está abaixo
+                                ?>Inactivo</td>
+                                <td>[editar][activar]</td>
+                                <?php
+                            }?>
+                        </td>
+                        </tr>
+                        <?php
+                    }?>
+                    <?php
+                }?>
                 </tbody>
             </table>
             <?php
@@ -188,7 +196,7 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
                 // Executa a query e guarda o retorno na variavel
                 $resultado_unidades = executa_query($query_unidades);
                 ?> <select name="unidade"> 
-                <option selected></option><?php
+                <option selected value="NULL"></option><?php
                 // Ciclo para percorrer todos os unidades e indicar os vários tipos de unidades
                 while ($array_unidades = mysqli_fetch_array($resultado_unidades)) {
                     $id = $array_unidades["id"];
@@ -208,8 +216,8 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
             </p>
             <p>
                 <label><b>Obrigatorio:</b></label>
-                    <input type="radio" name="obrigatorio" value="1"> <?php echo "Sim";?>
-                    <input type="radio" name="obrigatorio" value="0"> <?php echo "Não";?>
+                    <input type="radio" name="obrigatorio" value="sim"> <?php echo "Sim";?>
+                    <input type="radio" name="obrigatorio" value="nao"> <?php echo "Não";?>
             </p>
             <p>
                 <label><b>Objeto referenciado por este atributo:</b></label>
@@ -218,7 +226,7 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
                 // Executa a query e guarda o retorno na variavel
                 $resultado_objetos = executa_query($query_objetos);
                 ?> <select name="objeto_referenciado"> 
-                <option selected></option><?php
+                <option selected value="NULL"></option><?php
                 // Ciclo para percorrer todos os objetos e indicar os vários tipos de objeto
                 while ($array_objetos = mysqli_fetch_array($resultado_objetos)) {
                     $id = $array_objetos["id"];
@@ -243,7 +251,7 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
         $object_nome_do_atributo = guarda_variavel($_REQUEST['nome_do_atributo']);
         $object_tipo_de_valor = guarda_variavel($_REQUEST['tipo_de_valor']);
         $object_objeto = guarda_variavel($_REQUEST['objeto']);
-        $object_nome_formulario = guarda_variavel($_REQUEST['nome_formulario']);
+        $object_tipo_formulario = guarda_variavel($_REQUEST['nome_formulario']);
             $object_unidade = guarda_variavel($_REQUEST['unidade']);
         $object_ordem_formulario = guarda_variavel($_REQUEST['ordem_formulario']);
         $object_tamanho_formulario = guarda_variavel($_REQUEST['tamanho_formulario']);
@@ -266,7 +274,7 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
             <p>É necessário indicar um objeto.<p>
             <?php
             back();   
-        } elseif (empty($object_nome_formulario)) {
+        } elseif (empty($object_tipo_formulario)) {
             ?>
             <p>É necessário indicar um nome para o campo do formulário.<p>
             <?php
@@ -287,11 +295,29 @@ if (is_user_logged_in() && current_user_can('manage_attributes')) {
             <?php
             back();       
         } else {
-            echo "inseriu";?>
+            if($object_obrigatorio == "sim") {
+                $object_obrigatorio = 1;
+            } else {
+                $object_obrigatorio = 0;
+            }
+        
+            // código SQL em formato string para inserir novos atributos
+            $query_inserir = "INSERT INTO `attribute` (`id`, `name`, `value_type`, `obj_id`, `form_field_name`, `form_field_type`,
+            `unit_type_id`, `form_field_order`, `form_field_size`, `mandatory`, `obj_fk_id`) 
+            VALUES (NULL, '$object_nome_do_atributo', '$object_tipo_de_valor', '$object_objeto', 'tv-7-diagonal', '$object_tipo_formulario', " 
+            . $object_unidade . ", '$object_ordem_formulario', '$object_tamanho_formulario', '$object_obrigatorio', " . $object_objeto_referenciado . ")"; 
+
+            $result_insert = executa_query($query_inserir);
+ 
+            if ($result_insert) {
+            mysqli_query($liga,'COMMIT');
+            ?>
             <p>Inserção de dados feita com sucesso!
             Clique  em <a href="gestao-de-atributos">continuar</a> para avançar.
+            <!-- $string = preg_replace('/[^a-z0-9_ ]/i', '', $string);) -->
             <br/>
             <?php
+            }
         }
     }
         
